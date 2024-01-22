c59d537b210031386cd9a8becaeedd2d142f28a9
Daniel Kudinov <danielkudinov17@gmail.com>
2024-01-23 01:55:22 +0300

    add new style for page

--- kudinov/src/page/Main/index.jsx
+++ kudinov/src/page/Main/index.jsx
@@ -1,17 +1,44 @@
+import styled from 'styled-components';
+import img2 from '../../img/img2.png'
+
+const MainWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 100px 100px 100px 100px;
+    flex-direction: column;
+`
+const MainBlock = styled.div`
+    display: flex;
+    justify-content: space-around;
+    font-size: 30px;
+    padding-left: 75px;
+
+    .img{
+        /* height: 200px; */
+        transform: rotate(8deg);
+    }
+`
+const Block = styled.div`
+    display: flex;
+    flex-direction: column;
+`
 const main = () => {
     return ( 
         <>
-        <div>
-            <h1>
+        <MainWrapper>
+        <MainBlock>
+            <Block>
+            <h1 className="">
                 Кудинов Данил
             </h1>
-            <h2>
-                Fronted / Backend developer
-            </h2> 
-                
-            <img src="" alt="" />
-        </div>
-
+            <p>
+                Frontend and Backend developer
+            </p>
+            </Block>
+            <img className='img' src={img2} alt="" />
+        </MainBlock>
+        
+        </MainWrapper>
         </>
      );
 }
