480325a0d1b95c8a5a242951b8be43c241a345d9
Daniel Kudinov <danielkudinov17@gmail.com>
2024-02-04 20:47:54 +0300

    mob

--- kudinov/src/components/Footer/index.jsx
+++ kudinov/src/components/Footer/index.jsx
@@ -9,9 +9,7 @@ const Hr = styled.hr`
     opacity: 0.5;
     border: 0.1rem solid #ff8437;
     @media(max-width: 770px){
-        width: 500px;
-        opacity: 0.5;
-        border: 0.1rem solid #ff8437;
+        display: none;
     }
 `
 
@@ -19,14 +17,24 @@ const FooterWrapper = styled.nav`
     display: flex;
     justify-content: space-between;
     padding: 0px 220px;
+
+    @media(max-width: 770px){
+        gap: 245px;
+    }
 `
 const UlList = styled.ul`
     display: flex;
     list-style-type: none;
+    @media(max-width: 770px){
+        display: flex;
+    }
 `
 const Name = styled.p`
     color: grey;
     font-size: 15px;
+    @media(max-width: 770px){
+        display: flex;
+    }
 `
 const Footer = () => {
     return (
